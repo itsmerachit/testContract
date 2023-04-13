a985a7861c97193dfdb9553665f97f7b982e6672
Rachit Sharma <sharma.rachit2107@gmail.com>
2023-04-13 15:59:00 +0530

    Modified contracts

---
+++ contracts/Contract8.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract Contract8 {
+    function sayHello() public pure returns (string memory) {
+        return "Hello World";
+    }
+}
